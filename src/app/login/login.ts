4b7f0090a96cdbc38930260cba1b43a2d7b61937
Gaurav <gmkulkarni7499@gmail.com>
2026-03-13 22:03:23 +0530

    page2

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Page2 } from '../page2/page2';
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports: [Page2],
   templateUrl: './login.html',
   styleUrls: ['./login.css'],
 })
